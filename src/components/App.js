f598ccba91940b829114718883744ab53a4c4d05
nnastaran83 <nnastaran83@gmail.com>
2022-10-13 19:11:56 +0300

    axios

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,21 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
+class App extends React.Component {
+    onSearchSubmit(term) {
+        console.log(term);
+    }
 
-const App = () => {
-    return (
-        <div className="ui container" style={ { marginTop: '10px' } }>
-            <SearchBar />
-        </div>
-    );
+    render() {
+        return (
+            <div className="ui container" style={ { marginTop: '10px' } }>
+                <SearchBar onSubmit={ this.onSearchSubmit } />
+            </div>
+        );
+    }
+
 }
-
-
 
 
 export default App;
